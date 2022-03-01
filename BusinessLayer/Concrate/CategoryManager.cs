421a2e5d942d749bac0f891788189e7f95094605
Oguzhan SENLIK <raunn001@outlook.com>
2022-03-01 15:50:28 +0300

    Add project files.

---
+++ BusinessLayer/Concrate/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Concrate.Repositories;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrate
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBl()
+        {
+            return repo.List();
+        }
+
+        public void CategoryAddBl(Category p)
+        {
+            if (p.CategoryName.Length <= 3)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+
+            }
+        }
+    }
+}
